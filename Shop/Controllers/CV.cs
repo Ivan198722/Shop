884f5d5ee5bc7ee34d7987cbab20a216224fca26
Ivan <stadnik198722@gmail.com>
2024-10-31 10:59:15 +0100

    Release

---
+++ Shop/Controllers/CV.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CV : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
